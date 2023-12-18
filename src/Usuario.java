698e2674033ab2fc80aa8e815e139d77b2d9c04a
Etheron <40326651+Etheron8@users.noreply.github.com>
2023-12-18 11:59:50 -0300

    usando Scanner

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,20 +1,37 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Usuario {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        // criando objeto Scanner
 
-        SmartTv smartTv = new SmartTv();
-        System.err.println("Tv Ligada ?" + smartTv.ligada);
-        smartTv.mudarCanal(26);
-        System.err.println("Canal Atual : " + smartTv.canal);
-        System.err.println("Volume Atual : " + smartTv.volume);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        smartTv.ligar();
-        System.err.println("Novo Status  da Tv Ligada ?" + smartTv.ligada);
+        System.err.println("olá qual é seu nome ?");
+        String nome = scanner.next();
 
-        smartTv.desligar();
-        System.err.println("Novo Status  da Tv Ligada ?" + smartTv.ligada);
+        System.err.println("qual sua idade ?");
+        int idade = scanner.nextInt();
 
-        smartTv.aumentarVolume();
-        smartTv.aumentarVolume();
+        System.err.println("qual sua altura ?");
+        double altura = scanner.nextDouble();
 
+        System.err.println("Meu Nome é " + nome + " Minha idade é " + idade + " Minha Altura é " + altura);
+        /*
+         * SmartTv smartTv = new SmartTv();
+         * System.err.println("Tv Ligada ?" + smartTv.ligada);
+         * smartTv.mudarCanal(26);
+         * System.err.println("Canal Atual : " + smartTv.canal);
+         * System.err.println("Volume Atual : " + smartTv.volume);
+         * 
+         * smartTv.ligar();
+         * System.err.println("Novo Status  da Tv Ligada ?" + smartTv.ligada);
+         * 
+         * smartTv.desligar();
+         * System.err.println("Novo Status  da Tv Ligada ?" + smartTv.ligada);
+         * 
+         * smartTv.aumentarVolume();
+         * smartTv.aumentarVolume();
+         */
     }
 }
